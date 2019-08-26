6e3b728e79b28e532a3811e2a67043254eac06bc
kibnm821 <kibnm821@naver.com>
2019-08-26 15:55:01 +0900

    Park

--- src/main/java/com/example/learn_springboot/controller/FINALController.java
+++ src/main/java/com/example/learn_springboot/controller/FINALController.java
@@ -2,15 +2,11 @@ package com.example.learn_springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class FINALController {
-
-	@RequestMapping(value = {"/index", "/thymeleafmerge/thymeleafExtend"}, method = RequestMethod.GET)
+	@RequestMapping(value = {"/final/home"})
 	public void actionMethod() {
+		//return "Hellow";
 	}
 }
