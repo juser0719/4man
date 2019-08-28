f446100640a3bcf440a184a7ca999cfe880c4789
minsu-zip <alstn4814@gmail.com>
2019-08-28 10:10:42 +0900

    ㅏㅏㅏㅏ

--- src/main/java/com/example/learn_springboot/controller/FINALController12.java
+++ src/main/java/com/example/learn_springboot/controller/FINALController12.java
@@ -1,73 +1,73 @@
-package com.example.learn_springboot.controller;
+// package com.example.learn_springboot.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-
-
-@Controller
-public class FINALController12 {
-
-    String hidden="";
-    boolean A = false;
-
-    @RequestMapping(value = "/final/home")
-    public ModelAndView actionhome(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView) {
-
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-
-		if(paramMap.get("ID")== null){
-			A = false;
-		}else if(paramMap.get("ID")!= null){
-			A = true;
-		}
-
-        if(A){
-            hidden = (String) paramMap.get("ID");
-		}
-
-        if (hidden.equals("") || hidden.equals(false)) {
-            resultMap.put("ID", false);
-            resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인실패</button>");
-        } else {
-            resultMap.put("ID", hidden);
-            resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인성공</button>");
-        }
-
-        modelandView.addObject("resultMap", resultMap);
-        return modelandView;
-
-    }
-
-    @RequestMapping(value = "/final/{action}", method = { RequestMethod.GET, RequestMethod.POST })
-    public ModelAndView action01(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
-            ModelAndView modelandView) {
-        String viewName = "/final/";
-        Map<String, Object> resultMap = new HashMap<String, Object>();
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.servlet.ModelAndView;
 
 
-        hidden = (String) paramMap.get("ID");
 
-        if (hidden.equals("") || hidden.equals(false)) {
-            resultMap.put("ID", false);
-            resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인실패</button>");
+// @Controller
+// public class FINALController12 {
 
-        } else {
-            resultMap.put("ID", hidden);
-            resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인성공</button>");
-        }
+//     String hidden="";
+//     boolean A = false;
 
-        viewName += action;
-        modelandView.setViewName(viewName);
-        modelandView.addObject("resultMap", resultMap);
-        return modelandView;
-    }
+//     @RequestMapping(value = "/final/home")
+//     public ModelAndView actionhome(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView) {
 
-}
+//         Map<String, Object> resultMap = new HashMap<String, Object>();
+
+// 		if(paramMap.get("ID")== null){
+// 			A = false;
+// 		}else if(paramMap.get("ID")!= null){
+// 			A = true;
+// 		}
+
+//         if(A){
+//             hidden = (String) paramMap.get("ID");
+// 		}
+
+//         if (hidden.equals("") || hidden.equals(false)) {
+//             resultMap.put("ID", false);
+//             resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인실패</button>");
+//         } else {
+//             resultMap.put("ID", hidden);
+//             resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인성공</button>");
+//         }
+
+//         modelandView.addObject("resultMap", resultMap);
+//         return modelandView;
+
+//     }
+
+//     @RequestMapping(value = "/final/{action}", method = { RequestMethod.GET, RequestMethod.POST })
+//     public ModelAndView action01(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
+//             ModelAndView modelandView) {
+//         String viewName = "/final/";
+//         Map<String, Object> resultMap = new HashMap<String, Object>();
+
+
+//         hidden = (String) paramMap.get("ID");
+
+//         if (hidden.equals("") || hidden.equals(false)) {
+//             resultMap.put("ID", false);
+//             resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인실패</button>");
+
+//         } else {
+//             resultMap.put("ID", hidden);
+//             resultMap.put("VIEW", "<button type='button' class='btn btn-primary'>로그인성공</button>");
+//         }
+
+//         viewName += action;
+//         modelandView.setViewName(viewName);
+//         modelandView.addObject("resultMap", resultMap);
+//         return modelandView;
+//     }
+
+// }
---
+++ src/main/java/com/example/learn_springboot/controller/FINALController222.java
@@ -0,0 +1,71 @@
+package com.example.learn_springboot.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+
+@Controller
+public class FINALController222 {
+
+    String hidden="";
+    boolean A=true;
+    @RequestMapping(value = "/final/home")
+    public ModelAndView actionhome(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView) {
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        if(paramMap.get("ID")==null){
+            resultMap.put("VIEW", "<button>실패</button>");
+            resultMap.put("ID", "");
+        }
+        else if(!paramMap.get("ID").equals("")){
+            A = false;
+            hidden = (String)paramMap.get("ID");
+            resultMap.put("VIEW", "<button>성공</button>");
+            resultMap.put("ID", hidden);
+        }
+        else{
+
+        }
+
+            modelandView.addObject("resultMap", resultMap);
+            return modelandView;
+        
+
+    }
+
+    @RequestMapping(value = "/final/{action}", method = { RequestMethod.GET, RequestMethod.POST })
+    public ModelAndView action01(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
+            ModelAndView modelandView) {
+        String viewName = "/final/";
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+      
+
+        if(paramMap.get("ID")==null || paramMap.get("ID").equals("")){
+            resultMap.put("VIEW", "<button>실패</button>");
+            resultMap.put("ID", "");
+        }
+        else{
+            resultMap.put("VIEW", "<button>성공</button>");
+            resultMap.put("ID", hidden);
+        }
+
+        
+
+        viewName += action;
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultMap", resultMap);
+        return modelandView;
+    }
+
+}
