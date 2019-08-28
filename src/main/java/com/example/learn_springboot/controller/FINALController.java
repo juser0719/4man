f414b17a4bc4c38414daab72b4bbcd7a73192c01
kibnm821 <kibnm821@naver.com>
2019-08-28 17:25:55 +0900

    Park login patch

--- src/main/java/com/example/learn_springboot/controller/FINALController.java
+++ src/main/java/com/example/learn_springboot/controller/FINALController.java
@@ -19,28 +19,39 @@ public class FINALController {
 
     String hidden="";
     boolean A=true;
+    int n = 1;
     @RequestMapping(value = "/final/home")
     public ModelAndView actionhome(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView) {
 
         Map<String, Object> resultMap = new HashMap<String, Object>();
 
-        if(paramMap.get("ID")==null || paramMap.get("ID").equals("")){
-            resultMap.put("VIEW", "<button>실패</button>");
-            resultMap.put("ID", "");
+        if(paramMap.get("ID")!=null){//로그인 아웃 구분
+            n += 1;
+        }else if(paramMap.get("ID")==null && n >= 2){
+            n = 1;
         }
-        else if(!paramMap.get("ID").equals("")){
-            A = false;
-            hidden = (String)paramMap.get("ID");
+
+        if((paramMap.get("ID")==null || paramMap.get("ID").equals("")) && n == 1){//로그인 전
+            resultMap.put("ID", "");
+            resultMap.put("FORM1", "<button type='submit' name='ID' class='text-muted bg-white border-0 b'>Sign In</button>/");
+            resultMap.put("FORM2", "<button type='submit' class='text-muted bg-white border-0 b'>Sign Up</button>");
+            resultMap.put("VIEW", "");
+        }
+        else if((paramMap.get("ID")!=null || !paramMap.get("ID").equals("")) && n != 1){//로그인 후
+            resultMap.put("FORM1", "");
+            resultMap.put("FORM2", "");
             resultMap.put("VIEW", "<button>성공</button>");
             resultMap.put("ID", hidden);
-        }
-        else{
-
-        }
-
+        }//else if(!paramMap.get("ID").equals("") && n != 1){
+        //     A = false;
+        //     hidden = (String)paramMap.get("ID");
+        //     resultMap.put("FORM1", "");
+        //     resultMap.put("FORM1", "");
+        //     resultMap.put("VIEW", "<button>성공</button>");
+        //     resultMap.put("ID", hidden);
+        // }
             modelandView.addObject("resultMap", resultMap);
             return modelandView;
-        
 
     }
 
@@ -49,18 +60,26 @@ public class FINALController {
             ModelAndView modelandView) {
         String viewName = "/final/";
         Map<String, Object> resultMap = new HashMap<String, Object>();
-      
 
-        if(paramMap.get("ID")==null || paramMap.get("ID").equals("")){
-            resultMap.put("VIEW", "<button>실패</button>");
+
+        if((paramMap.get("ID")==null || paramMap.get("ID").equals("")) && n == 1){//로그인 전
             resultMap.put("ID", "");
-        }
-        else{
+            resultMap.put("FORM1", "<button type='submit' name='ID' class='text-muted bg-white border-0 b'>Sign In</button>/");
+            resultMap.put("FORM2", "<button type='submit' class='text-muted bg-white border-0 b'>Sign Up</button>");
+            resultMap.put("VIEW", "");
+        }else if((paramMap.get("ID")==null || paramMap.get("ID").equals("")) && n != 1){//로그인 후
+            resultMap.put("FORM1", "");
+            resultMap.put("FORM2", "");
             resultMap.put("VIEW", "<button>성공</button>");
             resultMap.put("ID", hidden);
-        }
-
-        
+        }//else if(!paramMap.get("ID").equals("") && n != 1){
+        //     A = false;
+        //     hidden = (String)paramMap.get("ID");
+        //     resultMap.put("FORM1", "");
+        //     resultMap.put("FORM1", "");
+        //     resultMap.put("VIEW", "<button>성공</button>");
+        //     resultMap.put("ID", hidden);
+        // }
 
         viewName += action;
         modelandView.setViewName(viewName);
